7eb5c75fc0b3afdb81ecfff10a043e6e5771344a
Dmitrii Levachev <Axel69rus@yandex.ru>
2015-10-28 11:02:40 +0300

    another test6346

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,9 +1,3 @@
-import static jdk.nashorn.internal.objects.NativeRegExp.test;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
